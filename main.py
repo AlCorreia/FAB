d7678f735417a6c3d916adf0299a296ae9c6fef0
Chaim Correia <alvaro.chaim.correia@accenture.com>
2017-07-22 23:19:56 +0100

    Fixing indexing bug

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from model import Model
 
 def main(config):
 	if config['pre']['run']:
-		prepro_each(config=config, data_type='train',out_name='train') #to preprocess the train data
-		prepro_each(config=config, data_type='dev',out_name='dev') #to preprocess  the dev data
+		prepro_each(config=config, data_type='train', out_name='train') #to preprocess the train data
+		prepro_each(config=config, data_type='dev', out_name='dev') #to preprocess  the dev data
 	if config['model']['run']:
 		data = read_data(config,'dev',ref=False)
 		config = update_config(config, data) #update config with max_word_size, max_passage_size, embedded_vector
