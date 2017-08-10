edb4506ec0f15d41e76e7295b646df122907441f
jorgeluizms <jorge.silva93@gmail.com>
2017-08-10 00:16:38 -0300

    Self attention layer added

--- AttentionModel.py
+++ AttentionModel.py
@@ -9,15 +9,85 @@ import pdb
 from tensorflow.examples.tutorials.mnist import input_data
 
 
+
+VERY_LOW_NUMBER = -1e30
+
 Base_Frequency = 10000
 number_of_words=3
-embedding_size=10
+embedding_size=512 #MUST BE EVEN FOR ENCODER
 size_of_vocabulary=100
 batch_size=3
+multihead_size = 8
 word_zeros=np.zeros([1,embedding_size])
 embedding_dict=np.array(np.concatenate([word_zeros,np.random.rand(size_of_vocabulary,embedding_size)],axis=0), dtype=np.float32)
 
-paragraphs=[np.random.randint(1,100, size=np.random.randint(3,8)) for i in range(batch_size)]
+def encoder (x,q):
+	#Compute the number of words in passage and question
+	size_x = tf.shape(x)[-2]
+	size_q = tf.shape(q)[-2]
+	#Create a row vector with range(0,n) = [0,1,2,n-1], where n is the greatest size between x and q.
+	pos = tf.cast(tf.expand_dims(tf.range(tf.cond(tf.greater(size_x,size_q),lambda: size_x, lambda: size_q)), 1),tf.float32)
+	#Create a vector with all the exponents
+	exponents = tf.divide(tf.range(embedding_size/2),embedding_size/2-1)
+	#Power the base frequency by exponents
+	freq_PG = tf.expand_dims(tf.pow(1/Base_Frequency,exponents),0)
+
+	#Compute the encoder values
+	encoder_sin = tf.sin(tf.matmul(pos,freq_PG))
+	encoder_cos = tf.cos(tf.matmul(pos,freq_PG))
+
+	#Concatenate both values
+	encoder = tf.concat([encoder_sin,encoder_cos], axis = 1)
+
+	#Computes the encoder values for x and q
+	encoder_x = tf.slice(encoder,[0,0],[size_x,embedding_size])
+	encoder_q = tf.slice(encoder,[0,0],[size_q,embedding_size])
+
+	#Encoding x and q
+	x_encoded = tf.add(x, encoder_x)
+	q_encoded = tf.add(q, encoder_q)
+	return x_encoded, q_encoded
+
+
+def self_attention_layer(X, mask,scope=None):
+	#Self-Attention is defined as:  softmax(X*W_sym*X')*X*WV
+	with tf.variable_scope(scope):
+		#In order to get a symmetric matrix W_V, only its eigenvectors and eigenvalues are variables.
+		WQ_EigVec = tf.nn.l2_normalize(tf.get_variable(name = 'WQEigVec', shape = [embedding_size,embedding_size], dtype = tf.float32), dim = 0)
+		WQ_EigenVal = tf.get_variable(name = 'WGEigVal', shape = [embedding_size])
+		WV = tf.get_variable(name = 'WV', shape = [embedding_size,embedding_size], dtype = tf.float32)
+		
+		#W_sym = WQ_EigVec*EigenVal*EigVec
+		# x*EigVec
+		x_proj = tf.matmul(X,
+			tf.tile(tf.expand_dims(WQ_EigVec,0),[batch_size,1,1]))
+		# x*EigVec is split into multi_head_size smaller matrices
+		x_proj_split = tf.transpose(tf.split(x_proj,num_or_size_splits = multihead_size, axis = 2),[1,2,0,3])
+		# x*EigVec*EigVal computed
+		WQ_EigenVal_Split = tf.split(WQ_EigenVal,num_or_size_splits = multihead_size, axis = 0)
+		x_proj_scaled = tf.multiply(x_proj_split,WQ_EigenVal_Split)
+
+		#(x*EigVec) * (x*EigVec*EigVal)' 
+		logits = tf.matmul(tf.transpose(x_proj_split,[0,2,1,3]),tf.transpose(x_proj_scaled,[0,2,3,1]))
+		#Sofmatx with masking
+		softmax = tf.nn.softmax(
+				tf.add(
+					tf.divide(tf.transpose(logits,[1,0,2,3]),tf.sqrt(tf.cast(embedding_size,tf.float32))),
+					tf.multiply(1 - mask, VERY_LOW_NUMBER)
+					), dim = -1)
+		#Final mask is applied
+		softmax = tf.multiply (mask,softmax)
+		#Computed the new x vector accoring to weights from softmax
+		x_weighted = tf.matmul(softmax,tf.tile(tf.expand_dims(X,0),[multihead_size,1,1,1]))
+		#Because of multihead attention, WV must be split into multi_head_size smaller matrices
+		WV_Split = tf.split(WV, num_or_size_splits = multihead_size, axis = 1)
+		#x_weighted*Wv
+		x_weighted_proj = tf.matmul(x_weighted,tf.tile(tf.expand_dims(WV_Split,1),[1,batch_size,1,1]))
+		#Concatenate everything togeter
+		x_weighted_proj_concat = tf.concat(tf.unstack(x_weighted_proj, axis = 0), axis = 2)
+	return x_weighted_proj_concat
+
+paragraphs=[np.random.randint(1,100, size=np.random.randint(9,12)) for i in range(batch_size)]
 questions = [np.random.randint(1,100, size=i) for i in range(2,7,2)]
 
 def padding(seq):
@@ -42,34 +112,23 @@ x_mask = tf.cast(tf.sign(x),tf.float32)
 q_mask = tf.cast(tf.sign(q),tf.float32)
 
 #Mask matrices
-mask_matrix_xx=tf.sign(tf.matmul(tf.expand_dims(x,-1),tf.expand_dims(x,1)))
-mask_matrix_qq=tf.sign(tf.matmul(tf.expand_dims(q,-1),tf.expand_dims(q,1)))
-mask_matrix_xq=tf.sign(tf.matmul(tf.expand_dims(x,-1),tf.expand_dims(q,1)))
+mask_matrix_xx=tf.cast(tf.sign(tf.matmul(tf.expand_dims(x,-1),tf.expand_dims(x,1))),tf.float32)
+mask_matrix_qq=tf.cast(tf.sign(tf.matmul(tf.expand_dims(q,-1),tf.expand_dims(q,1))),tf.float32)
+mask_matrix_xq=tf.cast(tf.sign(tf.matmul(tf.expand_dims(x,-1),tf.expand_dims(q,1))),tf.float32)
 
 #Implementing encoder
-size_x = tf.shape(x)[-1]
-size_q = tf.shape(q)[-1]
-pos = tf.cast(tf.expand_dims(tf.range(size_x), 1),tf.float32)
-freq = tf.divide(tf.range(embedding_size/2),embedding_size/2-1)
-freq_PG = tf.expand_dims(tf.pow(1/Base_Frequency,freq),0)
-
-encoder_sin = tf.sin(tf.matmul(pos,freq_PG))
-encoder_cos = tf.cos(tf.matmul(pos,freq_PG))
-
-encoder_x = tf.concat([encoder_sin,encoder_cos], axis = 1)
-
-encoder_q = tf.slice(encoder_x,[0,0],[size_q,embedding_size])
-
-#Encoding x_input
-x_encoded = tf.add(x_input, encoder_x)
-q_encoded = tf.add(q_input, encoder_q)
-
+x_encoded, q_encoded = encoder (x_input,q_input)
+self_att_layer_qq = self_attention_layer(q_encoded, mask = mask_matrix_qq, scope = 'qq')
+self_att_layer_xx = self_attention_layer(x_encoded, mask = mask_matrix_xx, scope = 'xx')
+#self_att_layer_xx = cross_attention_layer(x_encoded,mask_matrix_xq)
+#Self-Attention
 
 # Launch the graph in a session.
 # Evaluate the tensor `c`.
 with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
     tf.global_variables_initializer().run()
-    print(sess.run([mask_matrix_xx],feed_dict={x : paragraphs, q: questions}))
+    pdb.set_trace()
+    np.shape(sess.run([self_att_layer_qq],feed_dict={x : paragraphs, q: questions}))
     pdb.set_trace()
     a=1
-		
+
