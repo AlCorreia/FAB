f1ced468270006b29d1d2c06bb858e29efe1c87a
jorgeluizms <jorge.silva93@gmail.com>
2017-09-14 17:16:47 -0300

    Bugs fixed in utils and main

--- main.py
+++ main.py
@@ -53,16 +53,16 @@ def main(config):
                 plot(X = steps, EM = [EM_train_plot,EM_dev_plot], F1 = [F1_train_plot, F1_dev_plot], save_dir = './plots/plot.png')
                 summary_EM = tf.Summary(value=[tf.Summary.Value(tag='EM', simple_value=EM_dev)])
                 summary_F1 = tf.Summary(value=[tf.Summary.Value(tag='F1', simple_value=F1_dev)])
-				model.dev_writer.add_summary(summary_F1, i)
-				model.dev_writer.add_summary(summary_EM, i)
-				#TODO Make this print more readable than now
-				print('\nF1:'+str(F1_dev)+' EM:'+str(EM_dev)+'\n')
-			if i % config['train']['steps_to_email'] == 0 and i>0:
-				send_mail(attach_dir = './plots/plot.png', subject = config['model']['name'])
-	#To check the exact match and F1 of the model for dev
-	if config['model']['evaluate_dev']:
-		EM_dev, F1_dev=evaluate(config,model,data_dev)
-		print('\nF1:'+str(F1_dev)+' EM:'+str(EM_dev)+'\n')
+                model.dev_writer.add_summary(summary_F1, i)
+                model.dev_writer.add_summary(summary_EM, i)
+                #TODO Make this print more readable than now
+                print('\nF1:'+str(F1_dev)+' EM:'+str(EM_dev)+'\n')
+            if i % config['train']['steps_to_email'] == 0 and i>0:
+                send_mail(attach_dir = './plots/plot.png', subject = config['model']['name'])
+    #To check the exact match and F1 of the model for dev
+    if config['model']['evaluate_dev']:
+        EM_dev, F1_dev=evaluate(config,model,data_dev)
+        print('\nF1:'+str(F1_dev)+' EM:'+str(EM_dev)+'\n')
 
 
 def evaluate(config, model, data_dev): #To check the exact match and F1 of the model
